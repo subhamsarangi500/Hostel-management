3b5071b0514b7c4209b50ee214133296c5404033
subhamsarangi500 <58003591+subhamsarangi500@users.noreply.github.com>
2021-07-13 21:53:12 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+</html>
+<div class="foot"><footer>
+<p> Prepared By : Subham Sarangi: 12018009019552 CSE3G_69 </br>Subham Sarangi: 12018009019552 CSE3G_69</br>Subham Sarangi: 12018009019552 CSE3G_69 </p>
+</footer> </div>
+
+
+<style> .foot{text-align: right; border: 1px solid black;}</style>
+</html>
